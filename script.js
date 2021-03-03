4f2fada6eb7f45b0c78aa6a369088e8bea9731f5
Zaiyai <77562107+Zaiyai@users.noreply.github.com>
2021-03-03 20:29:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const button = document.querySelector("button");
 
 button.addEventListener("click",
 "function(){ alert("PAKYU KA RJ") }")
+
+docker run \
+        -v ${{ github.workspace }}:/srv/jekyll -v ${{ github.workspace }}/_site:/srv/jekyll/_site \
+        jekyll/builder:latest /bin/bash -c "chmod 777 /srv/jekyll && jekyll build --future"
